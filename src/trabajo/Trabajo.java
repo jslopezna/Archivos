a52f34c61b566ea214702fcf019e55021765d565
Juan Esteban Muñoz <Juan Esteban Muoz@10.203.143.187>
2017-10-25 18:58:10 -0500

    v 1.2

--- src/trabajo/Trabajo.java
+++ src/trabajo/Trabajo.java
@@ -15,14 +15,14 @@ public class Trabajo {
         Administrador c = new Administrador ("C", 2);
         
         System.out.println("Ingrese el Número de Documento del Cliente");
-        
-        try { int x = s.nextInt();;
+        int x = 0;
+        try { x = s.nextInt();
         } catch(InputMismatchException e){
             System.out.println("El Número de Documento sólo puede estar compuesto por números enteros");
-        }
-        
+        }    
         
         b.crearCliente("a",x);
+        
         //a.addAdmin(c);
         //System.out.println(a.getAdmin().toString());
     }
